700d386eb2fa3a86046310ad4a54e31d4e51f423
lm-gomes <lm.moreiragomes@gmail.com>
2025-09-27 19:48:12 -0300

    requirements fixed

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def get_version():
   return globals()['__version__']
 
 
-with open("README.md", "r") as arq:
+with open("README.md", "r", encoding="utf-8") as arq:
   readme = arq.read()
 
 
@@ -32,14 +32,12 @@ setup(name="llm4time",
       python_requires=">=3.10",
       packages=find_packages(),
       install_requires=[
-          "lmstudio==1.3.0",
-          "numpy>=1.23.0,<2.2.5",
-          "openai>=1.40.0,<1.86.0",
-          "openpyxl>=3.0.10,<3.1.0",
-          "pandas>=2.0.0,<2.2.3",
-          "permetrics>=1.5.0,<2.0.0",
-          "plotly>=5.15.0,<6.1.0",
-          "scikit-learn>=1.3.0,<1.7.1",
-          "scipy>=1.10.0,<1.15.3",
-          "statsmodels>=0.14.0,<0.14.5"
+          "openpyxl>=3.1.0,<3.2.0",
+          "numpy>=1.23.0,<2.3.0",
+          "pandas>=2.0.0,<2.3.0",
+          "permetrics>=2.0.0,<2.1.0",
+          "plotly>=6.1.0,<6.2.0",
+          "scikit-learn>=1.7.1,<1.8.0",
+          "scipy>=1.15.3,<1.16.0",
+          "statsmodels>=0.14.5,<0.15.0"
       ])
